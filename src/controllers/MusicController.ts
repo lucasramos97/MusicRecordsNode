ae755d9cf1a3aa768a95d0aafe82bcecd4daa097
lucasramos97 <lucasramosdev@gmail.com>
2021-05-14 15:42:36 -0300

    Optimization to get just one instance of MusicService in MusicController

--- src/controllers/MusicController.ts
+++ src/controllers/MusicController.ts
@@ -4,13 +4,14 @@ import PaginatedQueryModel from '@models/PaginatedQueryModel';
 import MusicService from '@services/MusicService';
 import { Request, Response } from 'express';
 
+const musicService = new MusicService();
+
 export default class MusicController {
   public async getAllPagination(req: Request, res: Response):
   Promise<Response<PaginatedQueryModel<Music>>> {
     const page = req.query.page ? Number.parseInt(req.query.page.toString(), 10) : 0;
     const size = req.query.size ? Number.parseInt(req.query.size.toString(), 10) : 5;
 
-    const musicService = new MusicService();
     const result = await musicService.getAllPagination(page, size);
 
     return res.json(result);
@@ -20,7 +21,6 @@ export default class MusicController {
     const musicId = Number.parseInt(req.params.id, 10);
 
     try {
-      const musicService = new MusicService();
       const result = await musicService.getById(musicId);
 
       return res.status(200).json(result);
@@ -33,7 +33,6 @@ export default class MusicController {
     const music = req.body;
 
     try {
-      const musicService = new MusicService();
       const result = await musicService.save(music);
 
       return res.status(201).json(result);
@@ -47,7 +46,6 @@ export default class MusicController {
     const music = req.body;
 
     try {
-      const musicService = new MusicService();
       const result = await musicService.update(musicId, music);
 
       return res.status(200).json(result);
@@ -60,7 +58,6 @@ export default class MusicController {
     const musicId = Number.parseInt(req.params.id, 10);
 
     try {
-      const musicService = new MusicService();
       const result = await musicService.logicalDelete(musicId);
 
       return res.status(200).json(result);
